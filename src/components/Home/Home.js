d481b2400050c9e41a417e7b63e7004fcab98778
beginner-g <969925523@qq.com>
2017-09-18 21:24:23 +0800

    num

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,29 +9,35 @@ class Home extends Component {
       good:'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=490374653,3763965140&fm=27&gp=0.jpg',
       title:'脆皮朱古力蛋糕',
       price:72,
-      num:1,
-      buy:false,
-      tolato:'0.00'
+      num:0,
+      buy:false
     },
       {
       id:2,
       good:'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3463154630,851888036&fm=27&gp=0.jpg',
       title:'云石芝士蛋糕',
       price:89,
-      num:1,
-      buy:false,
-      tolato:'0.00'
+      num:0,
+      buy:false
     },
       {
       id:3,
       good:'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3419714750,517058921&fm=27&gp=0.jpg',
       title:'绿茶布朗尼蛋糕',
       price:56,
-      num:1,
-      buy:false,
-      tolato:'0.00'
+      num:0,
+      buy:false
+    },
+      {
+      id:4,
+      good:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2640805390,438696420&fm=200&gp=0.jpg',
+      title:'黑森林蛋糕',
+      price:22,
+      num:0,
+      buy:false
     }
-    ]
+  ],
+    tolato:0
   }
   complete=(t)=>{
     if(t){
@@ -51,16 +57,25 @@ class Home extends Component {
   handleSub=(val,t)=>{
     const newgoods1=this.state.goods
     console.log(val)
+    console.log(t)
     if(val==='-'){
-      if(t.num>0){
-        newgoods1.find(re=>re.id===t.id).num--
+        newgoods1.map(t=>{
+          if(t===t.id&&t.num!==0){
+            return {...t,num:t.num-1}
+          }
+          return t
+        })
         this.setState({
           goods:newgoods1
         })
-      }
     }
     if(val==='+'){
-      newgoods1.find(re=>re.id===t.id).num++
+      newgoods1.map(t=>{
+        if(t===t.id){
+          return {...t,num:t.num+1}
+        }
+        return t
+      })
       this.setState({
         goods:newgoods1
       })
